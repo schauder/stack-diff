cd7731e08e947e5017d7f7c72507f182fd2072d0
GitHub Action <action@github.com>
2022-07-01 08:55:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/schauder/stackdiff/services/MyProjectService.kt
+++ src/main/kotlin/com/github/schauder/stackdiff/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.schauder.stackdiff.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.schauder.stackdiff.MyBundle
 
 class MyProjectService(project: Project) {
 
